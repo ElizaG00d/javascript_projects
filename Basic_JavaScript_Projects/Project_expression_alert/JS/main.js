0a7c82328c53d747a05930f7d57f7b26df7d6022
ElizaG00d <gooden.laurel.232@outlook.com>
2022-11-08 01:30:51 -0600

    Initial commit

---
+++ Basic_JavaScript_Projects/Project_expression_alert/JS/main.js
@@ -0,0 +1,33 @@
+window.alert("Hello, World!"); //opens an alert window//
+
+document.write("Hello, World!"); //document.write prints the text displayed//
+
+var X = "Hello lovely world."; //assigns a variable//
+
+document.write(X); //prints the variable//
+
+var A = "It's a beautiful day!";
+
+window.alert(A); //alert window with assigned variable string//
+ 
+document.write("Ellie says, \"It's a lovely morning!\""); //backslashes allow "" special characters//
+
+document.write("We are the sum of a thousand things"
+    +", our existence is a simple one with a million possiblities."); //connecting a string together with +//
+
+var C = ("And don\'t forget to smile! "
+    +"It's good for you!"); //assigning variable with special character and + to connect string//
+ 
+document.write("Remember to stay on the ground!" + C); //prints string with assigned variable C connected to it//
+
+var Mammal = "Dog", Reptile = "Turtle", Bird = "Parrot", Fish = "Mackeral"; //assign multiple variables with one var//
+
+document.write(Mammal);
+
+4+5 //expression//
+
+//document push additions//
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string.";
+
+document.write(Sent1 + Sent2); 
