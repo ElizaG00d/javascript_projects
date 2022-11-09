01a761bce772ae1e0f04977bfac13e52d2b7c01b
ElizaG00d <gooden.laurel.232@outlook.com>
2022-11-09 15:46:17 -0600

    getElementById

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function first_function() {
+    var str = "This is a little sentence for you.";
+    document.getElementById("Button_Text").innerHTML = str;
+} //displays str on click of button in html//
+
+function second_function() {
+    var sentence = "This is another line of text ";
+    sentence += "and this is a connected one.";
+    document.getElementById("Txt_2").innerHTML = sentence;
+} //"innerHTML mods the content of an HTML element"//
